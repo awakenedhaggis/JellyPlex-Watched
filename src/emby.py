c6da76680cfbc210dd2e10f92f9ca6238f999b4b
Luis Garcia <git@luigi311.com>
2024-05-06 21:28:48 -0600

    Add support for emby

---
+++ src/emby.py
@@ -0,0 +1,19 @@
+from src.jellyfin_emby import JellyfinEmby
+
+class Emby(JellyfinEmby):
+    def __init__(self, baseurl, token):
+        authorization = (
+            'Emby Client="other", '
+            'Device="script", '
+            'DeviceId="script", '
+            'Version="0.0.0"'
+        )
+        headers = {
+            "Accept": "application/json",
+            "X-Emby-Token": token,
+            "X-Emby-Authorization": authorization,
+        }
+
+        super().__init__(server_type="Emby", baseurl=baseurl, token=token, headers=headers)  
+
+        
